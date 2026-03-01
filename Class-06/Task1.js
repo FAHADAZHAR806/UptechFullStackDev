a7a71c2b9644aa73a1a0b57b8ca78b1ef6040e57
FADI <fahadkiyani28@gmail.com>
2026-03-01 21:37:32 +0500

    practice

--- Class-06/Task1.js
+++ Class-06/Task1.js
@@ -120,3 +120,90 @@ console.log(count);
 
 // Task 9: Prime Number Checker
 // Check if a number is prime.
+let prime = 2;
+for (let i = 2; i < prime; i++) {
+  if (prime % 2 === 0) {
+    console.log("Not prime");
+    return;
+  }
+}
+console.log("prime");
+
+/* User enters amount. 
+• If balance sufficient → deduct amount 
+• Else → "Insufficient balance" */
+
+let balance = 7000;
+
+function withdraw(amount) {
+  if (amount <= balance) {
+    balance = balance - amount;
+    console.log("Withdrawal successful");
+    console.log("Remaining Balance:", balance);
+  } else {
+    console.log("Insufficient balance");
+  }
+}
+
+withdraw(7000);
+
+/*Check if password: 
+• At least 8 characters 
+• Contains number 
+• Contains uppercase letter */
+
+function checkPassword(password) {
+  if (password.length < 8) {
+    console.log("Password kam az kam 8 characters ka hona chahiye");
+    return;
+  }
+
+  let hasNumber = false;
+  let hasUppercase = false;
+
+  for (let i = 0; i < password.length; i++) {
+    let ch = password[i];
+
+    // Number check
+    if (ch >= "0" && ch <= "9") {
+      hasNumber = true;
+    }
+
+    // Uppercase check
+    if (ch >= "A" && ch <= "Z") {
+      hasUppercase = true;
+    }
+  }
+
+  if (!hasNumber) {
+    console.log("Password me kam az kam 1 number hona chahiye");
+    return;
+  }
+
+  if (!hasUppercase) {
+    console.log("Password me kam az kam 1 uppercase letter hona chahiye");
+    return;
+  }
+
+  console.log("Password valid hai ");
+}
+
+checkPassword("heLloh8k");
+
+/* FizzBuzz 
+From 1–50: 
+• Multiple of 3 → "Fizz" 
+• Multiple of 5 → "Buzz" 
+• Both → "FizzBuzz" */
+
+for (let i = 1; i <= 50; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
