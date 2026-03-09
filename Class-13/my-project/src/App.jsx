c89d620cec11b02837efbd94717756d207182894
FADI <fahadkiyani28@gmail.com>
2026-03-09 16:56:37 +0500

    react-installation

---
+++ Class-13/my-project/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+export default function App() {
+  return (
+    <div>
+      <h1 className="flex justify-center items-center h-[100vh] bg-red-500 bg-400">
+        Welcome to react
+      </h1>
+    </div>
+  );
+}
