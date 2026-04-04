63c92aa39052f653d92d37f0824aa393897d34e0
FADI <fahadkiyani28@gmail.com>
2026-04-04 23:42:02 +0500

    api-json

--- Node/src/controllers/postController.js
+++ Node/src/controllers/postController.js
@@ -1,8 +1,16 @@
+const data = {
+  name: "fahad",
+  age: 25,
+  isStudent: true,
+};
 const getPosts = (req, res) => {
   res.send("Posts File Execute successfully");
 };
 const getrandom = (req, res) => {
   res.send("Random Posts File Execute successfully");
 };
+const getdata = (req, res) => {
+  res.json(data);
+};
 
-module.exports = { getPosts, getrandom };
+module.exports = { getPosts, getrandom, getdata };
--- Node/src/routes/postRoute.js
+++ Node/src/routes/postRoute.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { getPosts, getrandom } = require("../controllers/postController");
+const {
+  getPosts,
+  getrandom,
+  getdata,
+} = require("../controllers/postController");
 // const { getrandom } = require("../controllers/postController");
 
 router.get("/", getPosts);
 router.get("/getrandom", getrandom);
+router.get("/data", getdata);
 
 module.exports = router;
